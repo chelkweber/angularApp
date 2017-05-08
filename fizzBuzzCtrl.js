d5bd4718c01004065903d9ea43d50ae6e35bbf60
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-08 14:21:26 -0400

    fizzbuzz array fix

--- fizzBuzzCtrl.js
+++ fizzBuzzCtrl.js
@@ -2,9 +2,7 @@ var app = angular.module('angularApp');
 
 app.controller('fizzBuzzCtrl', function($scope, fizzBuzzService) {
 	console.log('Fizz Buzz ctrl has loaded');
-
-	fizzBuzzService.setNum();
 	
-	$scope.numArray = fizzBuzzService.getNum();
+	$scope.numArray = fizzBuzzService.setNum();
 	console.log($scope.numArray);
 });
--- fizzBuzzService.js
+++ fizzBuzzService.js
@@ -3,29 +3,61 @@ var app = angular.module('angularApp');
 //create factory to return num object
 app.factory('fizzBuzzService', function() {
 	//set empty object for numbers
-	var num = {};
+	var num;
 	var numArray = [];
-	
+//	
+//	function fizzBuzzArray(i) {
+//		for (i=1; i<=101; i++) {
+//				if (i%3 === 0 && i%5 === 0) {
+//					num = "FizzBuzz";
+//				} else if (i%5 === 0) {
+//					num = "Buzz";
+//				} else if (i%3 === 0) {
+//					num === "Fizz";
+//				} else {
+//					num = i;
+//				}
+//				numArray.push(num);
+//			}
+//	}
+//	
+//	fizzBuzzArray();
+//	
+//			return {
+//			//pull words from entry from
+//			setWords: function(entryWords) {
+//				words = entryWords;
+//				console.log(words);
+//			},
+//			//send words to story
+//			getWords: function(){
+//				return words;
+//			}
+//		};
+//	
 	return {
 		//function to set number value with ;
 		setNum: function(i) {
-			for (i=1; i<100; i++) {
+			numArray = [];
+			for (i=1; i<=100; i++) {
 				if (i%3 === 0 && i%5 === 0) {
 					num = "FizzBuzz";
 				} else if (i%5 === 0) {
 					num = "Buzz";
 				} else if (i%3 === 0) {
-					num === "Fizz";
+					num = "Fizz";
 				} else {
 					num = i;
 				}
 				numArray.push(num);
 			}
-	},
-		//function to return number value
-		getNum: function() {
 			return numArray;
-		}
+	}
+//		//function to return number value
+//		getNum: function() {
+//			setNum();
+//			return numArray;
+//		}
 	}
 });
 
